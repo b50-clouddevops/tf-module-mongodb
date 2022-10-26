01a21842403f070d09e2ca01c1b184fb382d7659
mvpc-11232 <mvp2612@hotmail.com>
2022-10-26 06:50:30 +0530

    Auto Push Commit

--- data.tf
+++ data.tf
@@ -2,7 +2,9 @@ data "terraform_remote_state" "vpc" {
   backend = "s3"
 
   config = {
-
+    bucket = "b50-tf-state-bucket"
+    key    = "vpc/${var.ENV}/terrafom.tfstate"
+    region = "us-east-1"
   }
 }
 
