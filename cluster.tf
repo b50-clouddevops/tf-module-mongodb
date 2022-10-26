af1e7bef7a1674ebc779488fd9b0a62e4d1b0145
mvpc-11232 <mvp2612@hotmail.com>
2022-10-26 06:51:40 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -1,18 +1,18 @@
-# Creates DocDB Cluster
-resource "aws_docdb_cluster" "docdb" {
-  cluster_identifier      = "roboshop-${var.ENV}"
-  engine                  = "docdb"
-  master_username         = "admin1"
-  master_password         = "roboshop1"
-# True only during lab, in prod , we will take a snapshot and that time value will be false
-  skip_final_snapshot     = true
-}
+# # Creates DocDB Cluster
+# resource "aws_docdb_cluster" "docdb" {
+#   cluster_identifier      = "roboshop-${var.ENV}"
+#   engine                  = "docdb"
+#   master_username         = "admin1"
+#   master_password         = "roboshop1"
+# # True only during lab, in prod , we will take a snapshot and that time value will be false
+#   skip_final_snapshot     = true
+# }
 
 
 # Creates Subnet Group
 resource "aws_docdb_subnet_group" "docdb" {
   name       = "roboshop-${var.ENV}"
-  subnet_ids = [aws_subnet.frontend.id, aws_subnet.backend.id]
+  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
 
   tags = {
     Name = "My docdb subnet group"
