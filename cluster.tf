c81408858f561a6d3f680879c441ac06fbfe9afc
mvpc-11232 <mvp2612@hotmail.com>
2022-10-26 06:24:00 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -1,8 +1,8 @@
 resource "aws_docdb_cluster" "docdb" {
-  cluster_identifier      = "roboshop-${var.EVV}"
+  cluster_identifier      = "roboshop-${var.ENV}"
   engine                  = "docdb"
-  master_username         = ??????
-  master_password         = ??????
+  master_username         = "admin1"
+  master_password         = "roboshop1"
   backup_retention_period = 5
   preferred_backup_window = "07:00-09:00"
   skip_final_snapshot     = true
