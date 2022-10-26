cee28b5faeb7c633a035a255a8049b46074104fe
mvpc-11232 <mvp2612@hotmail.com>
2022-10-26 07:02:26 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -22,8 +22,8 @@ resource "aws_docdb_subnet_group" "docdb" {
 
 # Creats DocDB Cluster Instances and adds them to the cluster
 resource "aws_docdb_cluster_instance" "cluster_instances" {
-  count              = 2
-  identifier         = "docdb-cluster-demo-${count.index}"
-  cluster_identifier = aws_docdb_cluster.default.id
-  instance_class     = "db.r5.large"
+  count              = 1
+  identifier         = "roboshop-${var.ENV}"
+  cluster_identifier = aws_docdb_cluster.docdb.id
+  instance_class     = "db.t3.medium"
 }
