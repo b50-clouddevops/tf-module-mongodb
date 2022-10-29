b1fd11153bd95df2732ddf9d38f883ad3339b0cd
mvpc-11232 <mvp2612@hotmail.com>
2022-10-29 06:35:42 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -19,9 +19,10 @@ command = <<EOF
   cd /tmp/
   curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
   wget https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
-  mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile /tmp/rds-combined-ca-bundle.pem --username admin1 --password roboshop1
-  unzip mongodb.zip 
+  unzip -o mongodb.zip 
   cd mongodb-main 
+  mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile /tmp/rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < catalogue.js
+  mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile /tmp/rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < users.js
   
 EOF 
 
