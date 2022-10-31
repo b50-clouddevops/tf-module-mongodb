ea5d73f1a365f03393159fdd8cdf74e6c9aec999
mvpc-11232 <mvp2612@hotmail.com>
2022-10-31 06:07:50 +0530

    Auto Push Commit

--- route53.tf
+++ route53.tf
@@ -1,4 +1,4 @@
-resource "aws_route53_record" "record" {
+resource "aws_route53_record" "mongodb" {
   zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_ID
   name    = "mongodb-${var.ENV}.data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_NAME"
   type    = "CNAME"
