7b07630be856a52ca47ca664ddbcc443c9b5f50e
mvpc-11232 <mvp2612@hotmail.com>
2022-10-31 06:01:06 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -11,24 +11,6 @@ resource "aws_docdb_cluster" "docdb" {
 }
 
 
-resource "null_resource" "mongodb-schema" { 
-  depends_on = [aws_docdb_cluster.docdb]
-
-  provisioner "local-exec" {
-command = <<EOF
-  cd /tmp/
-  curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
-  wget https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
-  unzip -o mongodb.zip 
-  cd mongodb-main 
-  mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile /tmp/rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < catalogue.js
-  mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile /tmp/rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < users.js
-  
-EOF   
-  }
-}
-
-
 # Creates Subnet Group
 resource "aws_docdb_subnet_group" "docdb" {
   name       = "roboshop-mongo-${var.ENV}"
